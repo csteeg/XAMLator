7ffd0f20f77d0407dadb3a4244eec731facacbf4
chris <chris@careconnections.nl>
2018-12-07 09:59:08 +0100

    Preserve everything in dll from linking

--- XAMLator.Server.Droid/Properties/AssemblyInfo.cs
+++ XAMLator.Server.Droid/Properties/AssemblyInfo.cs
@@ -1,9 +1,10 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
+using Xamarin.Forms.Internals;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
-
+[assembly: Preserve]
 [assembly: AssemblyTitle("XAMLator.Server.Droid")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
